52c856b344347b842f8e9e36cce7cdaa68e789ea
ysoo <ysoo@wisc.edu>
2023-11-19 14:12:52 -0800

    Adding rays

---
+++ ray.h
@@ -0,0 +1,26 @@
+/**
+ *        t = -1    t = 0     t = 1     t = 2
+ *  <-------|---------|---------|---------|---->
+ *                    A         b
+ * 
+ * We need a ray class and a computation of what color is seen along the ray
+ * Ray can be thought as a function P(t) = A + t*b
+ * P is a 3D position along a line in 3D
+ * A is the ray origin and b is the ray direction
+*/
+
+#include "vec3.h"
+
+class ray {
+    public: 
+        vec3 origin;
+        vec3 direction;
+
+        ray() {}
+        ray(const vec3& _origin, const vec3& _direction) : origin(_origin), direction(_direction) {}
+
+        // Calculate a point along the ray at a given parameter t
+        vec3 at(double t) const {
+            return origin + direction * t;
+        }  
+};
--- vec3.h
+++ vec3.h
@@ -1,19 +1,29 @@
 class vec3 {
-public:
-    float x, y, z;
+    public:
+        float x, y, z;
 
-    vec3() : x(0.0f), y(0.0f), z(0.0f) {}
-    vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+        vec3() : x(0.0f), y(0.0f), z(0.0f) {}
+        vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
 
-    // Add other vector operations as needed (e.g., addition, subtraction, dot product, cross product)
+        // Add other vector operations as needed (e.g., addition, subtraction, dot product, cross product)
 
-    // Example addition operation
-    vec3 operator+(const vec3& other) const {
-        return vec3(x + other.x, y + other.y, z + other.z);
-    }
+        // scalar multiplication
+        vec3 operator*(const double t) const {
+            return vec3(x * t, y * t, z * t);
+        }
+        
+        // multiplication
+        vec3 operator*(const vec3& other) const {
+            return vec3(x * other.x, y * other.y, z * other.z);
+        }
 
-    // Example subtraction operation
-    vec3 operator-(const vec3& other) const {
-        return vec3(x - other.x, y - other.y, z - other.z);
-    }
+        // addition operation
+        vec3 operator+(const vec3& other) const {
+            return vec3(x + other.x, y + other.y, z + other.z);
+        }
+
+        // subtraction operation
+        vec3 operator-(const vec3& other) const {
+            return vec3(x - other.x, y - other.y, z - other.z);
+        }
 };
